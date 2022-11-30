577b1bdab064f16b59d9073570d72a29a9f89368
Capybaramijo <116186265+Capybaramijo@users.noreply.github.com>
2022-11-30 23:24:45 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,7 @@
+YP = {}
+
+YP.table = {
+    {name = "Police", dsc = "...", job = "police", jobgrade = 1}, 
+    {name = "Taxi", dsc = "...", job = "taxi", jobgrade = 1}, 
+    {name = "Ambulance", dsc = "...", job = "ambulance", jobgrade = 1}
+}
