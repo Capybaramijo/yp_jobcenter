3db3af5cbd429eba8a887dee75e09d500c3dfc66
Capybaramijo <116186265+Capybaramijo@users.noreply.github.com>
2022-11-30 23:26:43 +0100

    Delete config.lua

--- config.lua
+++
@@ -1,7 +0,0 @@
-YP = {}
-
-YP.table = {
-    {name = "Police", dsc = "...", job = "police", jobgrade = 1}, 
-    {name = "Taxi", dsc = "...", job = "taxi", jobgrade = 1}, 
-    {name = "Ambulance", dsc = "...", job = "ambulance", jobgrade = 1}
-}
